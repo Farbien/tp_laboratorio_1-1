4ecd21bb70e7f79721aed827f6aa4042e2f9889e
Lucas <mendozalucas001@gmail.com>
2021-09-23 22:59:34 -0300

    TP terminado

--- TP_[1]/src/TP_[1].c
+++ TP_[1]/src/TP_[1].c
@@ -35,8 +35,100 @@ int main(void)
 	setbuf(stdout, NULL);
 
 	int opcion;
+	float primerOperando;
+	float segundoOperando;
+	int banderaPrimerOperando;
+	int banderaSegundoOperando;
+	float suma;
+	float resta;
+	float producto;
+	int seguir;
 
-	opcion = MenuOpciones();
+	seguir = 1;
+	banderaPrimerOperando = 1;
+	banderaSegundoOperando = 1;
 
-	SwitchOpciones (opcion);
+	while (seguir == 1)
+	{
+		printf ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		printf ("---------------------------------------\n");
+		printf ("Menú de opciones: \n");
+
+		if (banderaPrimerOperando == 1)
+		{
+			printf ("1.Ingrese el primer operando: \n");
+			banderaPrimerOperando = 0;
+		}
+			else
+			{
+				printf ("1.Ingrese el primer operando: %.2f\n", primerOperando);
+			}
+
+		if (banderaSegundoOperando == 1)
+		{
+			printf ("2.Ingrese el segundo operando: \n");
+			banderaSegundoOperando = 0;
+		}
+			else
+			{
+				printf ("2.Ingrese el segundo operando: %.2f\n", segundoOperando);
+			}
+		printf ("3.Calcular todas las operaciones: \n");
+		printf ("4.Informar resultados \n");
+		printf ("5.Salir\n");
+		printf ("---------------------------------------\n");
+		printf ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		printf ("Seleccione una opción: ");
+		scanf ("%d", &opcion);
+
+		switch (opcion)
+			{
+			case 1:
+				primerOperando =  IngresarOperando("Ingrese el primer operando: ");
+				break;
+				seguir = 1;
+
+			case 2:
+				segundoOperando = IngresarOperando("Ingrese el segundo operando: ");
+				break;
+				seguir = 1;
+
+			case 3:
+				printf ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+				printf ("---------------------------------------\n");
+				printf ("\nCalcular operaciones: \n");
+				printf ("a)Calcular la suma (%.2f+%.2f)\n", primerOperando, segundoOperando);
+				printf ("b)Calcular la resta (%.2f-%.2f)\n", primerOperando, segundoOperando);
+				printf ("c)Calcular la division (%.2f/%.2f)\n", primerOperando, segundoOperando);
+				printf ("d)Calcular la multiplicacion (%.2f*%.2f)\n", primerOperando, segundoOperando);
+				printf ("e)Calcular el factorial de %.2f y el factorial de %.2f \n", primerOperando, segundoOperando);
+				printf ("\n---------------------------------------\n");
+				printf ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+				break;
+				seguir = 1;
+
+			case 4:
+				seguir = 1;
+
+				printf ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+				printf ("---------------------------------------\n");
+				printf ("Resultados: \n");
+				suma = Sumar (primerOperando, segundoOperando);
+				printf ("a)El resultado de %.2f + %.2f es %.2f\n", primerOperando, segundoOperando, suma);
+				resta = Restar (primerOperando, segundoOperando);
+				printf ("b)El resultado de %.2f - %.2f es %.2f\n", primerOperando, segundoOperando, resta);
+				Dividir (primerOperando, segundoOperando);
+				producto = Multiplicar (primerOperando, segundoOperando);
+				printf ("d)El resultado de %.2f*%.2f es: %.2f\n", primerOperando, segundoOperando, producto);
+				Factorial (primerOperando);
+				Factorial (segundoOperando);
+				printf ("---------------------------------------\n");
+				printf ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+				break;
+
+			case 5:
+				seguir = 0;
+				break;
+			}
+	}
 }
--- TP_[1]/src/biblioteca.h
+++ TP_[1]/src/biblioteca.h
@@ -1,11 +1,6 @@
 #ifndef BIBLIOTECA_H_
 #define BIBLIOTECA_H_
 
-void SwitchOpciones (int opcion);
-int MenuOpciones (void);
-int MenuOpciones1 (float numero1);
-int MenuOpciones2 (float numero1, float numero2);
-int MenuOpciones3(float numero1, float numero2);
 float IngresarOperando (char mensaje[]);
 float Sumar (float primerNumero, float segundoNumero);
 float Restar (float primerNumero, float segundoNumero);
